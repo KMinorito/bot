51c98be7ce295e6b246ae1da1d080c99b140ef17
KMinorito <63516121+KMinorito@users.noreply.github.com>
2020-04-11 21:07:49 +0200

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -1,29 +1,52 @@
-<?
+<?php 
 
-  $botToken - "1200459306:AAGLwtKTBtuzGqTUeuQrCzvuF5L0NJGgnGc";
-  $website = "https://api.telegram.org/bot".$botToken;
+$botToken = "1200459306:AAGLwtKTBtuzGqTUeuQrCzvuF5L0NJGgnGc";
 
-  $update = file_get_contents('php://input');
-  $update = json_decode($update, TRUE);
+$website = "https://api.telegram.org/bot".$botToken;
 
-  $chatId = $update["message"]["chat"]["id"];
-  $chatType = $update["message"]["chat"]["type"];
+$update = file_get_contents('php://input');
+$update = json_decode($update, TRUE);
+$modo = 0;
 
-  $message = $update["message"]["text"];
+$chatId = $update["message"]["chat"]["id"];
+$chatType = $update["message"]["chat"]["type"];
+$userId = $update["message"]['from']['id'];
+$firstname = $update["message"]['from']['username'];
+if ($firstname=="") {
+	$modo=1;
+	$firstname = $update["message"]['from']['first_name'];
+}
 
-  switch ($message) {
-    case '/ayuda':
-      $response = "Prueba";
-      sendMessage($chatId, $response);
-      break;
-    case '/nueva':
+if ($modo == 0) {
+	$firstname = "@".$firstname;
+}
 
-      break;
-  }
+$message = $update["message"]["text"];
 
-  function sendMessage($chatId, $message){
-    $url = %GLOBALS[website].'/sendMessage?chat_id='.$chatId.'&parse_mode=HTML&text='.urlencode($response);
-    file_get_contents($url);
-  }
+$agg = json_encode($update, JSON_PRETTY_PRINT);
+
+//Extraemos el Comando
+$arr = explode(' ',trim($message));
+$command = $arr[0];
+
+$message = substr(strstr($message," "), 1);
+
+//No requieren variables del usuario.
+switch ($command) {
+	case '/ayuda':
+		$response = "Tranquilo, estoy contigo.";
+		sendMessage($chatId, $response);
+		break;
+}
+
+
+
+function sendMessage($chatId, $response, $keyboard = NULL){
+	if (isset($keyboard)) {
+		$teclado = '&reply_markup={"keyboard":['.$keyboard.'], "resize_keyboard":true, "one_time_keyboard":true}';
+	}
+	$url = $GLOBALS[website].'/sendMessage?chat_id='.$chatId.'&parse_mode=HTML&text='.urlencode($response).$teclado;
+	file_get_contents($url);
+}
 
 ?>
